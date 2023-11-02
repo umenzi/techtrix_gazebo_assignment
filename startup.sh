79a6060fc0a6709cee9c041bef4c55ad8c0a8dd2
Dennis Heijmans <d.heijmans2019@gmail.com>
2023-11-02 11:09:19 +0100

    Wait a little in between launches

--- startup.sh
+++ startup.sh
@@ -2,5 +2,7 @@
 source /opt/ros/noetic/setup.bash
 source /catkin_ws/devel/setup.bash
 roslaunch techtrix_gazebo techtrix.launch &
+sleep 5
 roslaunch techtrix_gazebo techtrix_rviz.launch &
+sleep 5
 roslaunch techtrix_control techtrix_control.launch &
